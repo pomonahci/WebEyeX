bb5d27c473c0dc8ae20ed8a2e1719df3ffe5cce5
Kevin Lee <kevinsangholee@gmail.com>
2018-05-31 13:14:31 -0700

    GOT IT WORKINGish

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');                   //connect with express
 var path = require('path');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const io = require('socket.io')(http);
 var ss = require('socket.io-stream');
 var gazejs = require("gazejs");
 var Readable = require('stream').Readable;
@@ -11,59 +11,57 @@ app.use(express.static('scripts'))
 
 const port = 3000;
 
-// var stream = ss.createStream();
-var s = new Readable();
-s._read = function () {};
+io.of('/data').on('connection', socket => {
+  console.log('connected'); 
 
-// ss.emit('data', stream);
+  socket.on('disconnected', () => {
+    console.log("disconnected");
+  })
 
-function sendData(){
-  stream.write("hello")
-}
+  var timer = setInterval(function() {
+    var stream = ss.createStream();
+    var s = new Readable();
+    s._read = function() {};
+    s.push("Hello");
+    s.pipe(stream);
+    ss(socket).emit('gaze', stream, "yeah");
+    console.log("Pushed to stream.");
+  }, 1000);
 
-var n = 0;
- io.of('/data').on('connection', function(socket) {
-    if(n==0){
-      ss(socket).emit('data', stream);
-      n++;
-      console.log("emitting");
-    }
 });
 
+// var eyeTracker = gazejs.createEyeTracker(gazejs.TOBII_GAZE_SDK);//or gazejs.SR_EYELINK_SDK
+// var listener = {
+//     onConnect:function(){
+//         console.log("Library version: "+eyeTracker.getLibraryVersion());
+//         console.log("Model name: "+eyeTracker.getModelName());
 
+//         eyeTracker.start();
+//         console.log("OnConnect");
+//     },
+//     onStart:function(){
+//         console.log("OnStart");
+//     },
+//     onStop:function(){
+//         console.log("OnStop");
+//     },
+//     onError:function(error){
+//         console.log(error);
+//     },
+//     onGazeData:function(gazeData){
+//         if('prefered' in gazeData) {
+//           console.log('data');
+//           sendData()
+//         }
+//     }
+// };
 
-var eyeTracker = gazejs.createEyeTracker(gazejs.TOBII_GAZE_SDK);//or gazejs.SR_EYELINK_SDK
-var listener = {
-    onConnect:function(){
-        console.log("Library version: "+eyeTracker.getLibraryVersion());
-        console.log("Model name: "+eyeTracker.getModelName());
+// io.on('stop', function() {
+//   eyeTracker.release();
+// })
 
-        eyeTracker.start();
-        console.log("OnConnect");
-    },
-    onStart:function(){
-        console.log("OnStart");
-    },
-    onStop:function(){
-        console.log("OnStop");
-    },
-    onError:function(error){
-        console.log(error);
-    },
-    onGazeData:function(gazeData){
-        if('prefered' in gazeData) {
-          console.log('data');
-          sendData()
-        }
-    }
-};
-
-io.on('stop', function() {
-  eyeTracker.release();
-})
-
-eyeTracker.setListener(listener);
-eyeTracker.connect();
+// eyeTracker.setListener(listener);
+// eyeTracker.connect();
 
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'index.html'));
---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+var ss = require('socket.io-stream');
+
+
+$(function(){
+        var socket = io.connect('http://localhost:3000/data');
+        socket.on('connect', function() {
+          console.log("Connected to server.");
+
+          ss(socket).on('gaze', function(stream, data) {
+            console.log(data);
+            console.log(stream);
+            stream.on('data', function(data2) {
+              dataString = '';
+              dataString += data2;
+              console.log(dataString);
+            })
+          })
+        });
+
+        var canvas=document.getElementById("canvas");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        document.body.scrollTop = 0;
+        document.body.style.overflow = 'hidden';
+        var eye=canvas.getContext("2d");
+        var num = 0;
+
+        var canvasOffset=$("#canvas").offset();
+        var offsetX=canvasOffset.left;
+        var offsetY=canvasOffset.top;
+
+        function draw(x,y){
+            eye.clearRect(0,0,canvas.width,canvas.height);
+            eye.beginPath();
+            eye.fillStyle="red";
+            eye.rect(x,y,10,10);
+            eye.fill();
+            eye.stroke();
+        }
+
+        function handleMouseDown(e){
+
+        }
+
+        $("#canvas").mousedown(function(e){handleMouseDown(e);});
+
+        // socket.on('gazeData', function(data){
+        //   if(data !== null) {
+        //   prefX = data.x * screen.width;
+        //   prefY = data.y * screen.height - (window.outerHeight - window.innerHeight);
+        //   console.log("recieved: "+ num);
+        //   num++;
+        //   draw(prefX, prefY);
+        // }
+        // });
+
+}); // end $(function(){});
